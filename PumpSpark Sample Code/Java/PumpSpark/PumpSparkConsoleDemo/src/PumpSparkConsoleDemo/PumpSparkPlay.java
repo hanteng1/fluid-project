c17f7cb6f17bc95f5006f28f1dc855500ec0697f
Teng Han <hanteng1021@outlook.com>
2018-01-15 13:35:17 -0500

    added some notations

--- PumpSpark Sample Code/Java/PumpSpark/PumpSparkConsoleDemo/src/PumpSparkConsoleDemo/PumpSparkPlay.java
+++ PumpSpark Sample Code/Java/PumpSpark/PumpSparkConsoleDemo/src/PumpSparkConsoleDemo/PumpSparkPlay.java
@@ -7,6 +7,9 @@ public class PumpSparkPlay extends PApplet {
 	
 	PumpSparkManager pumpSpark;
 	
+	private boolean oneOn = false;
+	private boolean twoOn = false;
+	private boolean threeOn = false;
 	
 	public void settings()
 	{
@@ -35,19 +38,90 @@ public class PumpSparkPlay extends PApplet {
 		if (key == 'q') {
 			pumpSpark.actuatePump((byte)0, (byte)0);
 			pumpSpark.actuatePump((byte)1, (byte)0);
+			pumpSpark.actuatePump((byte)2, (byte)0);
+		
+			oneOn = false;
+			twoOn = false;
+			threeOn = false;
+			println("pump 1 is off");
+			println("pump 2 is off");
+			println("pump 3 is off");
+			
 			delay(100);
+			pumpSpark.disconnectPort();
 			exit();
 		}else if(key == 'b')
 		{
 			pumpSpark.actuatePump((byte)0, (byte)254);
 			pumpSpark.actuatePump((byte)1, (byte)254);
 			pumpSpark.actuatePump((byte)2, (byte)254);
-		}else if(key == 'e')
+			
+			oneOn = true;
+			twoOn = true;
+			threeOn = true;
+			println("pump 1 is on");
+			println("pump 2 is on");
+			println("pump 3 is on");
+			
+		}else if(key == '1') {
+			if(oneOn)
+			{
+				pumpSpark.actuatePump((byte)0, (byte)0);
+				println("pump 1 is off");
+			}else
+			{
+				pumpSpark.actuatePump((byte)0, (byte)254);
+				println("pump 1 is on");
+			}
+			oneOn = !oneOn;
+		}else if(key == '2')
+		{
+			if(twoOn)
+			{
+				pumpSpark.actuatePump((byte)1, (byte)0);
+				println("pump 2 is off");
+			}else
+			{
+				pumpSpark.actuatePump((byte)1, (byte)254);
+				println("pump 2 is on");
+			}
+			twoOn = !twoOn;
+		}else if(key == '3')
+		{
+			if(threeOn)
+			{
+				pumpSpark.actuatePump((byte)2, (byte)0);
+				println("pump 3 is off");
+			}else
+			{
+				pumpSpark.actuatePump((byte)2, (byte)254);
+				println("pump 1 is on");
+			}
+			threeOn = !threeOn;
+		}
+		else if(key == 'e')
 		{
 			pumpSpark.actuatePump((byte)0, (byte)0);
 			pumpSpark.actuatePump((byte)1, (byte)0);
 			pumpSpark.actuatePump((byte)2, (byte)0);
+			
+			oneOn = false;
+			twoOn = false;
+			threeOn = false;
+			println("pump 1 is off");
+			println("pump 2 is off");
+			println("pump 3 is off");
+		}else if(key == 'f')
+		{
+			pumpSpark.actuatePump((byte)3, (byte)254);
+			
+			delay(2000);
+			
+			pumpSpark.actuatePump((byte)3, (byte)0);
 		}
+		
+		
+		
 	}
 	
 	
