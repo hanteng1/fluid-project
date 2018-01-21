e36bb10c747e45aea23a9f066a5c6fe27f9436b2
Teng Han <hanteng1021@outlook.com>
2018-01-21 18:36:27 -0500

    had a gui

--- PumpSpark Sample Code/Java/PumpSpark/PumpSparkConsoleDemo/src/PumpSparkConsoleDemo/PumpSparkPlay.java
+++ PumpSpark Sample Code/Java/PumpSpark/PumpSparkConsoleDemo/src/PumpSparkConsoleDemo/PumpSparkPlay.java
@@ -11,9 +11,26 @@ public class PumpSparkPlay extends PApplet {
 	private boolean twoOn = false;
 	private boolean threeOn = false;
 	
+	//buttons 
+	int[] rectXs;
+	int[] rectYs;
+	int rectX, rectY;
+	int numButtons;
+	int rectWidth;
+	int rectHeight;
+	int rectColor, rectHighlight;
+	int rectOverIndex = -1;
+	
+	int windowWidth, windowHeight;
+	int mouseTriggered = -1;
+	
+	String[] buttonTexts;
+	
 	public void settings()
 	{
-		size(300, 300);
+		windowWidth = 1000;
+		windowHeight = 1000;
+		size(windowWidth, windowHeight);
 	}
 	
 	public void setup()
@@ -23,14 +40,88 @@ public class PumpSparkPlay extends PApplet {
 		pumpSpark.configurePort("COM7");	
 		// Open the serial port
 		pumpSpark.connectPort();
+		
+		rectColor = color(211, 211, 211);
+		rectHighlight = color(105, 105, 105);
+		
+		rectWidth = (int) (windowWidth * 0.8);
+		rectX = (windowWidth - rectWidth) / 2;
+		//5 buttons
+		numButtons = 5;
+		rectXs = new int[] {rectX, rectX, rectX, rectX, rectX};
+		rectHeight = 100;
+		rectYs = new int[] {(int)(rectHeight * 1.5), (int)(rectHeight * 3.0), (int)(rectHeight * 4.5), (int)(rectHeight * 6.0), (int)(rectHeight * 7.5)};
+		
+		buttonTexts = new String[] {"Beating", "Vibration", "x", "x", "x"};
 	}
 	
 	public void draw()
 	{
-		//pumpSpark.actuatePump((byte)0, (byte)254);
-		//delay(5000);
-		//pumpSpark.actuatePump((byte)0, (byte)0);
-		//delay(5000);
+		update(mouseX, mouseY);
+		
+		for(int itrr = 0; itrr < numButtons; itrr++)
+		{
+			if(mouseTriggered == itrr)
+			{
+				fill(rectHighlight);
+			}else
+			{
+				fill(rectColor);
+			}
+			
+			rect(rectXs[itrr], rectYs[itrr], rectWidth, rectHeight);
+			
+			textSize(32);
+			fill(0, 102, 153);
+			
+			text(buttonTexts[itrr], rectXs[itrr] + rectWidth / 2 - textWidth(buttonTexts[itrr]) / 2, rectYs[itrr] + 2 * rectHeight / 3); 
+			
+		}
+	}
+	
+	private void update(int x, int y)
+	{
+		for(int itrr = 0; itrr < numButtons; itrr++)
+		{
+			if(overRect(rectXs[itrr], rectYs[itrr], rectWidth, rectHeight))
+			{
+				rectOverIndex = itrr;
+				return;
+			}
+		}
+		
+		rectOverIndex = -1;
+	}
+	
+	private boolean overRect(int x, int y, int width, int height)
+	{
+		if (mouseX >= x && mouseX <= x+width && 
+			      mouseY >= y && mouseY <= y+height) {
+		    return true;
+		} 
+		else {
+			return false;
+		}
+	}
+	
+	public void mousePressed()
+	{
+		if(rectOverIndex >= 0)
+		{
+			if(mouseTriggered >= 0)
+			{
+				if(mouseTriggered == rectOverIndex)
+				{
+					mouseTriggered = -1;
+				}
+				
+			}else
+			{
+				mouseTriggered = rectOverIndex;
+				
+				println("" + mouseTriggered);
+			}
+		}
 	}
 	
 	
@@ -113,11 +204,22 @@ public class PumpSparkPlay extends PApplet {
 			println("pump 3 is off");
 		}else if(key == 'f')
 		{
-			pumpSpark.actuatePump((byte)3, (byte)254);
+			pumpSpark.actuatePump((byte)0, (byte)254);
+			pumpSpark.actuatePump((byte)1, (byte)254);
+			pumpSpark.actuatePump((byte)2, (byte)254);
+			println("pump 1 is on");
+			println("pump 2 is on");
+			println("pump 3 is on");
 			
 			delay(2000);
+			println("2 seconds");
 			
-			pumpSpark.actuatePump((byte)3, (byte)0);
+			pumpSpark.actuatePump((byte)0, (byte)0);
+			pumpSpark.actuatePump((byte)1, (byte)0);
+			pumpSpark.actuatePump((byte)2, (byte)0);
+			println("pump 1 is off");
+			println("pump 2 is off");
+			println("pump 3 is off");
 		}
 		
 		
